cb39fa0de1e0a95c0cc2b3e92df423363bfb1126
OmarKHerj <omarkherjouj@gmail.com>
2023-03-17 07:55:15 -0700

    good

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-	char alpha[24] = "abcdfghijklmnoprstuvwxyz";
-	int x;
-	for (x = 0; x < 24; x++)
-	{
-		putchar(alpha[x]);
-	}
-	putchar('\n');
-	return (0);
+char alpha[24] = "abcdfghijklmnoprstuvwxyz";
+int x;
+for (x = 0; x < 24; x++)
+{
+putchar(alpha[x]);
+}
+putchar('\n');
+return (0);
 }
