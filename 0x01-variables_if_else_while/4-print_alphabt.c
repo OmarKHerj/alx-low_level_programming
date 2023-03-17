632c5974fe0828f3eef7fd8ba26b9e8361c514cb
OmarKHerj <omarkherjouj@gmail.com>
2023-03-17 07:51:49 -0700

    good

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Prints the alphabet except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha[24] = "abcdfghijklmnoprstuvwxyz";
+	int x;
+	for (x = 0; x < 24; x++)
+	{
+		putchar(alpha[x]);
+	}
+	putchar('\n');
+	return (0);
+}
