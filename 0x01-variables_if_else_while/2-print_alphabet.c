604737e4d076076051e5234253ec2c85a15bad71
OmarKHerj <omarkherjouj@gmail.com>
2023-03-17 06:59:31 -0700

    C languague

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - print alphabet in lower cases
+ *
+ * return: return 0 
+ */
+int main(void)
+{
+char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+int x;
+for (x = 0; x < 26; x++)
+{
+	putchar(alpha[x]);
+}
+putchar("\n");
+return (0);
+}
