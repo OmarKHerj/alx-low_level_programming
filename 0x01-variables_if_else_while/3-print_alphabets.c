292944508f2ad3fe6421966054f00564cc89d077
OmarKHerj <omarkherjouj@gmail.com>
2023-03-17 07:29:39 -0700

    C languague

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,13 @@ int main(void)
 char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
 char beta[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 int x;
-for (x = 0,x < 26,x++)
+for (x = 0;x < 26;x++)
 {
 putchar(alpha[x]);
 }
-for (x = 0,x <26,x++)
+for (x = 0;x <26;x++)
 {
-putchar(beta[x];
+putchar(beta[x]);
 }
 return 0
 }	
