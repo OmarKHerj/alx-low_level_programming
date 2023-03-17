fba8ab27777d714d2284ccf83853e88e9f1f269a
OmarKHerj <omarkherjouj@gmail.com>
2023-03-17 07:28:00 -0700

    C languague

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) 
+{
+char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+char beta[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int x;
+for (x = 0,x < 26,x++)
+{
+putchar(alpha[x]);
+}
+for (x = 0,x <26,x++)
+{
+putchar(beta[x];
+}
+return 0
+}	
