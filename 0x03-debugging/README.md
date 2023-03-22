5d283089c418db5b5ba142a01e6694f6c1916ac2
OmarKHerj <omarkherjouj@gmail.com>
2023-03-22 01:35:35 -0700

    debugging

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+this direcrtory about debugging
---
+++ 0x03-debugging/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - tests function that prints if integer is positive or negative or zero
+ * Return: 0
+ */
+int main(void)
+{
+int x;
+x = 0;
+positive_or_negative(x);
+return (0);
+}
