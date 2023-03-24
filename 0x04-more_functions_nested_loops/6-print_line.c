cf24d92680c400427f9c97881fc5c8d2f8fd14a0
OmarKHerj <omarkherjouj@gmail.com>
2023-03-23 17:22:28 -0700

    functions

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 
 /**
  * print_line - prints straight line n times.
@@ -8,10 +8,16 @@
 void print_line(int n)
 {
 int x;
+if (x = 0)
+{
+_putchar('\n');
+}
+else
+{
 for (x = 0; x < n; x++)
 {
 _putchar(95);
 }
 _putchar('\n');
 }
-
+}
