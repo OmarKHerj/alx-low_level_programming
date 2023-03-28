d8807450ee9bab42e4902bfafbe27e1f88871f1d
OmarKHerj <omarkherjouj@gmail.com>
2023-03-27 17:25:03 -0700

    pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,12 +8,13 @@
  */
 void print_array(int *a, int n)
 {
-	int x;
+	int x = 0;
+
 	for (; x < n; x++)
 	{
 		printf("%d", *(a + x));
 		if (x != (n - 1))
-			printf(", ");
+		printf(", ");
 	}
 	printf("\n");
 }
