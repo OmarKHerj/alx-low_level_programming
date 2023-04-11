903618be3e689bd7bc02f5d4223c97783c67e7b0
OmarKHerj <omarkherjouj@gmail.com>
2023-04-11 15:32:31 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,34 +8,36 @@
  *
  * Return: pointer of an array of chars
  */
- char *str_concat(char *s1, char *s2)
+char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
-	unsigned int x, y, k, len;
+	unsigned int x, y, z, len;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-    for (x = 0; s1[x] != '\0'; x++)
+
+	for (x = 0; s1[x] != '\0'; x++)
 		;
 
 	for (y = 0; s2[y] != '\0'; y++)
 		;
-    k = x + y;
-	s = malloc((sizeof(char) * l) + 1);
+
+	ptr = malloc(sizeof(char) * (x + y + 1));
+
 	if (ptr == NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
-	for (k = 0; k < x; k++)
-		ptr[k] = s1[k];
+	for (z = 0; z < x; z++)
+		ptr[z] = s1[z];
 
 	len = y;
-	for (y = 0; y <= len; k++, y++)
-		ptr[k] = s2[y];
+	for (y = 0; y <= len; z++, y++)
+		ptr[z] = s2[y];
 
 	return (ptr);
 }
