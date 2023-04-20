60b3b94ebe0d9bb24eef318f7459e56d4d0503d4
OmarKHerj <omarkherjouj@gmail.com>
2023-04-19 17:34:41 -0700

    funtion pointers

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
  * struct op - Struct op
  *
@@ -22,4 +23,4 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
-#endif // CALC_H
+#endif
