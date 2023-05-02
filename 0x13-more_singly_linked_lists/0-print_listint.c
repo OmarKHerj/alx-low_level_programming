f3e6c0c7156734491a4b0f902946fb40c94332d9
OmarKHerj <omarkherjouj@gmail.com>
2023-05-02 12:20:33 -0700

    more

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,17 +8,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	int numb = 0;
+        int numb = 0;
 
-	if (h != NULL)
-	{
-		while (h)
-		{
-			printf("%d\n", h->n);
-			h = h->next;
-			numb++;
-		}
-	}
+        if (h != NULL) {
+                while (h) {
+                        printf("%d\n", h->n);
+                        h = h->next;
+                        numb++;
+                }
+        }
 
-	return (numb);
+        return (numb);
 }
