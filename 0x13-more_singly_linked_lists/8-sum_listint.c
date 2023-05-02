44ab424de99731790dd9f56ae05b9bd8894c4bd0
OmarKHerj <omarkherjouj@gmail.com>
2023-05-02 13:10:18 -0700

    singly linked list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * sum_listint - returns the sum of all the data (n) 
+ * sum_listint - returns the sum of all the data (n)
  * @head: head of the list
  *
  * Return: the sum
