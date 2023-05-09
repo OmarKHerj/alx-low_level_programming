58cd6d44178d67d89055529bc5b538bb10013db6
OmarKHerj <omarkherjouj@gmail.com>
2023-05-09 15:53:54 -0700

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,18 +3,18 @@
 /**
  * error_file - checks if files can be opened
  * @file_from: .....
- * @file_toward: ....
+ * @file_to: ....
  * @argv: arguments vector.
  * Return: no return.
  */
-void error_file(int file_from, int file_toward, char *argv[])
+void error_file(int file_from, int file_to, char *argv[])
 {
 if (file_from == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-if (file_toward == -1)
+if (file_to == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
@@ -29,19 +29,19 @@ exit(99);
  */
 int main(int argc, char *argv[])
 {
-int file_from, file_toward, shutdown_error;
+int file_from, file_to, shutdown_error;
 ssize_t rd, wr;
 char buffer[1024];
 
 if (argc != 3)
 {
-dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_toward");
+dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 exit(97);
 }
 
 file_from = open(argv[1], O_RDONLY);
-file_toward = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-error_file(file_from, file_toward, argv);
+file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+error_file(file_from, file_to, argv);
 
 rd = 1024;
 while (rd == 1024)
@@ -49,7 +49,7 @@ while (rd == 1024)
 rd = read(file_from, buffer, 1024);
 if (rd == -1)
 error_file(-1, 0, argv);
-wr = write(file_toward, buffer, rd);
+wr = write(file_to, buffer, rd);
 if (wr == -1)
 error_file(0, -1, argv);
 }
@@ -61,7 +61,7 @@ dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 exit(100);
 }
 
-shutdown_error = close(file_toward);
+shutdown_error = close(file_to);
 if (shutdown_error == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
